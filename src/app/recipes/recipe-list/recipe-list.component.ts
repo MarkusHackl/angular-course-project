e0d9e23c6b539e0c2c21d96807a4f4f65a1f470c
MarkusHackl <markus-hackl@hotmail.com>
2024-06-17 21:51:28 +0200

    components & Databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,11 +7,22 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
       'https://3.bp.blogspot.com/-LRe4HNCJAMk/WM8EKOAwDkI/AAAAAAAADMg/SwynQgjwz1gdg6IfI5g3Ggdvc_XhFYvwgCLcB/s640/IMG_5831.JPG'
     ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply a test',
+      'https://3.bp.blogspot.com/-LRe4HNCJAMk/WM8EKOAwDkI/AAAAAAAADMg/SwynQgjwz1gdg6IfI5g3Ggdvc_XhFYvwgCLcB/s640/IMG_5831.JPG'
+    ),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.css'
+  styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() incredientAdded = new EventEmitter<Ingredient>();
 
+  onAddItem() {
+    this.incredientAdded.emit(
+      new Ingredient(
+        this.nameInputRef.nativeElement.value,
+        this.amountInputRef.nativeElement.value
+      )
+    );
+    console.log('Test');
+  }
 }
